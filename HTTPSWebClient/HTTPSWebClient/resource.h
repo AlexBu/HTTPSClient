bedb66311461b80f5f180eb78bcd7331339d931e
Alex Bu <bkpfx000@gmail.com>
2012-09-17 17:28:12 +0800

    add basic ide code

---
+++ HTTPSWebClient/HTTPSWebClient/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by HTTPSWebClient.rc
+//
+#define IDD_HTTPSWEBCLIENT_DIALOG       102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT_URL                    1000
+#define IDC_EDIT_POST                   1001
+#define IDC_EDIT_GET                    1002
+#define IDC_BUTTON_POST                 1003
+#define IDC_BUTTON_GET                  1004
+#define IDC_STATIC_VALICODEPIC          1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
