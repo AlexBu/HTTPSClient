a533c887fdc95c2b264c3f0f22f3b02383d96318
Alex Bu <bkpfx000@gmail.com>
2013-01-22 14:20:54 +0800

    one file

--- HTTPSWebClient/HTTPSWebClient/Page.h
+++ HTTPSWebClient/HTTPSWebClient/Page.h
@@ -5,15 +5,18 @@
 #include "Log.h"
 #include "Utility.h"
 
-#define ERROR_OK		(0)
-#define ERROR_GENERAL	(-1)
-#define ERROR_HTTP		(-2)
-#define ERROR_LOGIC		(-3)
-#define ERROR_VALIDATE	(-4)
-#define ERROR_USERNAME	(-5)
-#define ERROR_PASSWORD	(-6)
-#define ERROR_OVERLOAD	(-7)
-#define ERROR_NOSEAT	(-8)
+#define ERROR_OK				(0)
+#define ERROR_GENERAL			(-1)
+#define ERROR_HTTP				(-2)
+#define ERROR_LOGIC				(-3)
+#define ERROR_VALIDATE			(-4)
+#define ERROR_USERNAME			(-5)
+#define ERROR_PASSWORD			(-6)
+#define ERROR_OVERLOAD			(-7)
+#define ERROR_NOSEAT			(-8)
+#define ERROR_ORDER				(-9)
+#define ERROR_CANCEL_TOO_MANY	(-10)
+#define ERROR_REAL_NAME			(-11)
 
 #define MAX_DATA_SIZE (1*1024*1024L)
 
